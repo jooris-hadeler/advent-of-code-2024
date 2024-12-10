ce2fd4b68d8a8f58a840fb3a79562beb7060f470
Jooris Hadeler <j.hadeler@outlook.com>
2024-12-10 20:37:44 +0100

    day 09 optimization

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -6,71 +6,33 @@ enum Cell {
     Empty,
 }
 
-impl Cell {
-    pub fn is_empty(&self) -> bool {
-        match self {
-            Cell::File(_) => false,
-            Cell::Empty => true,
-        }
-    }
-
-    pub fn is_file(&self) -> bool {
-        match self {
-            Cell::File(_) => true,
-            Cell::Empty => false,
-        }
-    }
-
-    pub fn as_file(&self) -> usize {
-        match self {
-            &Cell::File(id) => id,
-            Cell::Empty => panic!(),
-        }
-    }
-}
-
-fn load() -> (Vec<Cell>, usize) {
+fn load() -> Vec<Cell> {
     let input = include_str!("input.txt");
 
     let mut id_counter = 0;
 
-    (
-        input
-            .trim()
-            .chars()
-            .map(|ch| ch.to_digit(10).expect("failed to convert char") as usize)
-            .enumerate()
-            .flat_map(|(idx, length)| {
-                let id = id_counter;
+    input
+        .trim()
+        .chars()
+        .map(|ch| ch.to_digit(10).expect("failed to convert char") as usize)
+        .enumerate()
+        .flat_map(|(idx, length)| {
+            let id = id_counter;
 
-                if idx % 2 == 0 {
-                    id_counter += 1;
-                    repeat_n(Cell::File(id), length)
-                } else {
-                    repeat_n(Cell::Empty, length)
-                }
-            })
-            .collect(),
-        id_counter,
-    )
-}
-
-fn find_index_and_length_by_id(map: &Vec<Cell>, id: usize) -> Option<(usize, usize)> {
-    let mut iter = map.iter().enumerate().rev();
-
-    let (end_idx, _) = iter.find(|(_, cell)| cell.is_file() && cell.as_file() == id)?;
-
-    let length = iter
-        .take_while(|(_, cell)| cell.is_file() && cell.as_file() == id)
-        .count();
-
-    Some((end_idx - length, length + 1))
+            if idx % 2 == 0 {
+                id_counter += 1;
+                repeat_n(Cell::File(id), length)
+            } else {
+                repeat_n(Cell::Empty, length)
+            }
+        })
+        .collect()
 }
 
 fn main() {
     let load_start = Instant::now();
 
-    let (disk_map, max_file_id) = load();
+    let disk_map = load();
 
     let load_end = Instant::now();
 
@@ -88,7 +50,7 @@ fn main() {
     while let Some(cell) = part_1_disk_map.pop() {
         iterations += 1;
 
-        if cell.is_empty() {
+        if matches!(cell, Cell::Empty) {
             continue;
         }
 
@@ -96,7 +58,7 @@ fn main() {
             .iter_mut()
             .skip(last_empty_index)
             .enumerate()
-            .find(|(_, cell)| cell.is_empty())
+            .find(|(_, cell)| matches!(cell, Cell::Empty))
         {
             Some((idx, empty_cell)) => {
                 *empty_cell = cell;
@@ -128,61 +90,90 @@ fn main() {
 
     let mut part_2_disk_map = disk_map;
 
-    let mut empty_spaces = {
-        let mut empty_spaces = Vec::new();
+    let mut empty_spaces = Vec::new();
+    let mut file_spaces = Vec::new();
 
-        let mut current_start = None;
-        let mut current_length: usize = 0;
+    let mut empty_start = None;
+    let mut empty_length: usize = 0;
 
-        for (i, cell) in part_2_disk_map.iter().enumerate() {
-            match cell {
-                Cell::Empty => {
-                    if current_start.is_none() {
-                        current_start = Some(i); // Start a new block
-                    }
-                    current_length += 1;
+    let mut file_start = None;
+    let mut file_id = 0;
+    let mut file_length: usize = 0;
+
+    for (i, cell) in part_2_disk_map.iter().enumerate() {
+        match cell {
+            Cell::Empty => {
+                if let Some(file_start) = file_start.take() {
+                    file_spaces.push((file_start, file_length));
                 }
-                _ => {
-                    // Reset block tracking on encountering a non-empty cell
-                    if let Some(current_start) = current_start.take() {
-                        empty_spaces.push((current_start, current_length));
-                    }
 
-                    current_length = 0;
+                // Start a new empty block
+                if empty_start.is_none() {
+                    empty_start = Some(i);
+                    empty_length = 0;
                 }
+
+                empty_length += 1;
+            }
+            &Cell::File(id) => {
+                if let Some(empty_start) = empty_start.take() {
+                    empty_spaces.push((empty_start, empty_length));
+                }
+
+                if id != file_id {
+                    if let Some(file_start) = file_start.take() {
+                        file_spaces.push((file_start, file_length));
+                        file_length = 0;
+                    }
+                }
+
+                // Start a new file block
+                if file_start.is_none() {
+                    file_start = Some(i);
+                    file_length = 0;
+                    file_id = id;
+                }
+
+                file_length += 1;
             }
         }
+    }
 
-        empty_spaces
-    };
+    if let Some(file_start) = file_start.take() {
+        file_spaces.push((file_start, file_length));
+    }
 
-    for file_id in (0..max_file_id).rev() {
-        let (file_index, file_length) =
-            find_index_and_length_by_id(&part_2_disk_map, file_id).expect("failed to find file");
+    if let Some(empty_start) = empty_start.take() {
+        empty_spaces.push((empty_start, empty_length));
+    }
 
-        let Some((idx, (empty_index, empty_length))) = empty_spaces
+    while let Some((file_index, file_length)) = file_spaces.pop() {
+        let result = empty_spaces
             .iter()
             .copied()
-            .enumerate()
-            .find(|&(_, (_, empty_length))| empty_length >= file_length)
-        else {
+            .position(|(_, empty_length)| empty_length >= file_length);
+
+        if result.is_none() {
             continue;
-        };
+        }
+
+        let idx = result.unwrap();
+        let (empty_index, empty_length) = empty_spaces[idx];
 
         if empty_index > file_index {
             continue;
         }
 
-        let left_over = empty_length - file_length;
+        let left_over_empty_space = empty_length - file_length;
 
         for i in 0..file_length {
             part_2_disk_map.swap(file_index + i, empty_index + i);
         }
 
-        if left_over > 0 {
-            empty_spaces[idx] = (empty_index + file_length, left_over);
+        if left_over_empty_space > 0 {
+            empty_spaces[idx] = (empty_index + file_length, left_over_empty_space);
         } else {
-            empty_spaces.remove(idx);
+            empty_spaces[idx] = (usize::MAX, 0);
         }
     }
 
